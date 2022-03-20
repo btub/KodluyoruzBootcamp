5bb808a501bfc6a3932570721d0c9a595ba5d9de
buse t kocabıyık <btubakocabiyik@gmail.com>
2022-03-20 15:11:25 +0300

    Add Odev2

---
+++ Odev2a/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Odev2a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            bool prime = true;
+
+            Console.Write("Number:");
+
+            try
+            {
+                number = Convert.ToInt16(Console.Read());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid value.");
+            };
+
+            for (int i = 2; i < Math.Sqrt(number); i++)
+            {
+                if(number%i == 0)
+                    prime = false;
+            }
+
+            if (prime)
+                Console.WriteLine("Prime!");
+            else
+                Console.WriteLine("Not prime!");
+        }
+    }
+}
