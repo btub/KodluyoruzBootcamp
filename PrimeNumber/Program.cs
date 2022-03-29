62733974e0d1a077d4d1769959b0fa3480280c34
buse t kocabıyık <btubakocabiyik@gmail.com>
2022-03-29 18:55:31 +0300

    Updated

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            bool prime = true;
+
+        Start:
+            Console.Write("Number:");
+
+            try
+            {
+                number = Convert.ToInt16(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid value.");
+                goto Start;
+            };
+
+            for (int i = 2; i < Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    prime = false;
+            }
+
+            if (number == 1)
+                prime = false;
+
+            if (prime)
+                Console.WriteLine("Prime!");
+            else
+                Console.WriteLine("Not prime!");
+        }
+    }
+}
---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool prime;
+            Console.WriteLine(2);
+            for (int i = 3; i < 10000; i += 2)
+            {
+                prime = true;
+                for (int j = 2; j < Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime)
+                    Console.WriteLine(i);
+            }
+    }
+}
