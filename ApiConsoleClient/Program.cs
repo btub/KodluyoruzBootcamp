8bbc0bdc211a91ead7241c56cd91cc64c6e24fd3
buse t kocabıyık <btubakocabiyik@gmail.com>
2022-05-04 16:49:28 +0300

    Console Api Client

---
+++ ApiConsoleClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace ApiConsoleClient
+{
+    internal class Program
+    {
+        static async void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://localhost:7065/api/products");
+            
+            if(response.IsSuccessStatusCode)
+            {
+                var products = await response.Content.ReadFromJsonAsync<List<Product>>();
+            }
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int CategoryId { get; set; }
+        public string? ImageUrl { get; set; }
+        public double Discount { get; set; }
+        public int Stock { get; set; }
+    }
+}
